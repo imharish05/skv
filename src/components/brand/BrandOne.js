942e1f217edf44768ee5d4dd1ac9dbdf9c6dc0ac
imharish05 <harish05082004@gmail.com>
2026-03-29 15:02:48 +0530

    change in the certificates

--- src/components/brand/BrandOne.js
+++ src/components/brand/BrandOne.js
@@ -1,129 +1,74 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, FreeMode } from "swiper"; // Ensure these modules are imported
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/autoplay";
 
 export default class BrandOne extends React.Component {
-    render(){
-        let publicUrl = process.env.PUBLIC_URL+'/'
+    render() {
+        let publicUrl = process.env.PUBLIC_URL + '/';
+        
+        // Array for your 3 unique certificates
+        const certificates = [
+            { id: 1, img: "certificate1.png" },
+            { id: 2, img: "certificate2.png" },
+            { id: 3, img: "certificate3.png" },
+        ];
+
         return (
-            <>
-                <section className="brand-one-sec">
-                    <div className="brand-one__bg" style={{backgroundImage: 'url('+publicUrl+'assets/images/backgrounds/brand-v1-bg.png)'}}></div>
-                    <div className="container">
-                        <Swiper 
-                            loop={true}
-                            spaceBetween={50}
-                            slidesPerView={5}
-                            speed={1400}
-                            freeMode={true}
-                            watchSlidesVisibility={true}
-                            watchSlidesProgress={true}
-                            autoplay={{delay:   5000}}
-                            breakpoints={{
-                                "0": {
-                                    spaceBetween: 30,
-                                    slidesPerView: 1
-                                },
-                                375: {
-                                    spaceBetween: 30,
-                                    slidesPerView: 2
-                                },
-                                575: {
-                                    spaceBetween: 30,
-                                    slidesPerView: 2
-                                },
-                                767: {
-                                    spaceBetween: 30,
-                                    slidesPerView: 3
-                                },
-                                991: {
-                                    spaceBetween: 30,
-                                    slidesPerView: 4
-                                },
-                                1199: {
-                                    spaceBetween: 30,
-                                    slidesPerView: 4
-                                }
-                            }}
-                        className="thm-swiper__slider swiper-container">
-                            <div className="swiper-wrapper">
-
-                                {/* Start Brand One Single */}
-                                <SwiperSlide className="swiper-slide">
-                                    <Link to={process.env.PUBLIC_URL + `/`}><img src={publicUrl+"assets/images/brand/brand-v1-img1.png"} alt="" /></Link>
-                                </SwiperSlide>
-                                {/* End Brand One Single */}
-                                
-                                {/* Start Brand One Single */}
-                                <SwiperSlide className="swiper-slide">
-                                    <Link to={process.env.PUBLIC_URL + `/`}><img src={publicUrl+"assets/images/brand/brand-v1-img2.png"} alt="" /></Link>
-                                </SwiperSlide>
-                                {/* End Brand One Single */}
-                                
-                                {/* Start Brand One Single */}
-                                <SwiperSlide className="swiper-slide">
-                                    <Link to={process.env.PUBLIC_URL + `/`}><img src={publicUrl+"assets/images/brand/brand-v1-img3.png"} alt="" /></Link>
-                                </SwiperSlide>
-                                {/* End Brand One Single */}
-                                
-                                {/* Start Brand One Single */}
-                                <SwiperSlide className="swiper-slide">
-                                    <Link to={process.env.PUBLIC_URL + `/`}><img src={publicUrl+"assets/images/brand/brand-v1-img4.png"} alt="" /></Link>
-                                </SwiperSlide>
-                                {/* End Brand One Single */}
-
-                                {/* Start Brand One Single */}
-                                <SwiperSlide className="swiper-slide">
-                                    <Link to={process.env.PUBLIC_URL + `/`}><img src={publicUrl+"assets/images/brand/brand-v1-img1.png"} alt="" /></Link>
-                                </SwiperSlide>
-                                {/* End Brand One Single */}
-
-                                {/* Start Brand One Single */}
-                                <SwiperSlide className="swiper-slide">
-                                    <Link to={process.env.PUBLIC_URL + `/`}><img src={publicUrl+"assets/images/brand/brand-v1-img2.png"} alt="" /></Link>
-                                </SwiperSlide>
-                                {/* End Brand One Single */}
-
-                                {/* Start Brand One Single */}
-                                <SwiperSlide className="swiper-slide">
-                                    <Link to={process.env.PUBLIC_URL + `/`}><img src={publicUrl+"assets/images/brand/brand-v1-img3.png"} alt="" /></Link>
-                                </SwiperSlide>
-                                {/* End Brand One Single */}
-
-                                {/* Start Brand One Single */}
-                                <SwiperSlide className="swiper-slide">
-                                    <Link to={process.env.PUBLIC_URL + `/`}><img src={publicUrl+"assets/images/brand/brand-v1-img4.png"} alt="" /></Link>
-                                </SwiperSlide>
-                                {/* End Brand One Single */}
-
-                                {/* Start Brand One Single */}
-                                <SwiperSlide className="swiper-slide">
-                                    <Link to={process.env.PUBLIC_URL + `/`}><img src={publicUrl+"assets/images/brand/brand-v1-img1.png"} alt="" /></Link>
-                                </SwiperSlide>
-                                {/* End Brand One Single */}
-
-                                {/* Start Brand One Single */}
-                                <SwiperSlide className="swiper-slide">
-                                    <Link to={process.env.PUBLIC_URL + `/`}><img src={publicUrl+"assets/images/brand/brand-v1-img2.png"} alt="" /></Link>
-                                </SwiperSlide>
-                                {/* End Brand One Single */}
-
-                                {/* Start Brand One Single */}
-                                <SwiperSlide className="swiper-slide">
-                                    <Link to={process.env.PUBLIC_URL + `/`}><img src={publicUrl+"assets/images/brand/brand-v1-img3.png"} alt="" /></Link>
-                                </SwiperSlide>
-                                {/* End Brand One Single */}
-
-                                {/* Start Brand One Single */}
-                                <SwiperSlide className="swiper-slide">
-                                    <Link to={process.env.PUBLIC_URL + `/`}><img src={publicUrl+"assets/images/brand/brand-v1-img4.png"} alt="" /></Link>
-                                </SwiperSlide>
-                                {/* End Brand One Single */}
-                            </div>
-                        </Swiper>
-                    </div>
-                </section>
-            </>
-        )
+            <section className="brand-one-sec py-5">
+                <div className="brand-one__bg" style={{ backgroundImage: `url(${publicUrl}assets/images/backgrounds/brand-v1-bg.png)` }}></div>
+                <div className="container">
+                    <Swiper
+                        modules={[Autoplay, FreeMode]}
+                        loop={true}
+                        spaceBetween={30}
+                        slidesPerView={3} // Default for desktop
+                        speed={1400}
+                        freeMode={true}
+                        autoplay={{
+                            delay: 3000,
+                            disableOnInteraction: false,
+                        }}
+                        breakpoints={{
+                            // Mobile: 1 image
+                            0: {
+                                slidesPerView: 1,
+                                spaceBetween: 20,
+                            },
+                            // Tablet: 2 images
+                            576: {
+                                slidesPerView: 2,
+                                spaceBetween: 30,
+                            },
+                            // Desktop: 3 images
+                            992: {
+                                slidesPerView: 3,
+                                spaceBetween: 40,
+                            }
+                        }}
+                        className="thm-swiper__slider"
+                    >
+                        {certificates.map((item) => (
+                            <SwiperSlide key={item.id}>
+                                <div className="brand-one__single p-2 bg-white rounded">
+                                    <Link to="/">
+                                        <img 
+                                            className='img-fluid w-50' 
+                                            src={`${publicUrl}assets/images/certificates/${item.img}`} 
+                                            alt={`Certificate ${item.id}`} 
+                                        />
+                                    </Link>
+                                </div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </div>
+            </section>
+        );
     }
 }
